71930a8a6675353ff7084f0a29c13e5d2ab3f6ae
iris <iriskurien@gmail.com>
2024-11-01 23:28:47 +0530

    added icons

--- maze.py
+++ maze.py
@@ -1,12 +1,22 @@
 from flask import Flask, render_template
+import random
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    grid_size = 5
-    maze = [[' ' for _ in range(grid_size)] for _ in range(grid_size)]
-    return render_template('index.html', maze=maze)
+     maze = [[' ' for _ in range(5)] for _ in range(5)] 
+     maze[0][0] = 'static/door.png'
+     maze[4][4] = 'static/flag.png'
+
+     skulls_to_place = 5  
+     while skulls_to_place > 0:
+        row = random.randint(0, 4)
+        col = random.randint(0, 4)
+        if maze[row][col] == ' ':  
+            maze[row][col] = 'static/banana.png'
+            skulls_to_place -= 1
+     return render_template('index.html', maze=maze)
 
 if __name__ == '__main__':
     app.run(debug=True)
