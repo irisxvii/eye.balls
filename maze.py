8f0819183cd1f2d9ebdf86f6c812465d2067c11d
iris <iriskurien@gmail.com>
2024-11-02 17:13:14 +0530

    added animation, made grid 7*7

--- maze.py
+++ maze.py
@@ -5,14 +5,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    maze = [[' ' for _ in range(5)] for _ in range(5)]
+    maze = [[' ' for _ in range(7)] for _ in range(7)]
     maze[0][0] = 'static/door.png' 
-    maze[4][4] = 'static/flag.png'  
+    maze[6][6] = 'static/flag.png'  
 
-    skulls_to_place = 7
+    skulls_to_place = 16
     while skulls_to_place > 0:
-        row = random.randint(0, 4)
-        col = random.randint(0, 4)
+        row = random.randint(0, 6)
+        col = random.randint(0, 6)
         if maze[row][col] == ' ': 
             maze[row][col] = 'static/bhooth.png'
             skulls_to_place -= 1
